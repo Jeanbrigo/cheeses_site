716b2f0afa8da0f7e5fc24f1a65ba8de887f5552
Jean Brigonnet <jeanbrigo@gmail.com>
2023-01-10 20:47:14 -0600

    frontend done

---
+++ src/loaders.js
@@ -0,0 +1,12 @@
+const URL = "https://people-api-o78q.onrender.com"
+
+export const peopleLoader = async () => {
+  const response = await fetch(URL + "/people")
+  const people = await response.json()
+  return people
+}
+export const personLoader = async ({params}) => {
+  const response = await fetch(URL + "/people/" + params.id )
+  const person = await response.json()
+  return person
+}
