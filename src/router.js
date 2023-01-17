40c0427fdd86d40beb20327f161005f17b0ea0e9
Jean Brigonnet <jeanbrigo@gmail.com>
2023-01-16 23:37:31 -0600

    cleanup code

--- src/router.js
+++ src/router.js
@@ -11,9 +11,10 @@ import {
   
   const router = createBrowserRouter(
     createRoutesFromElements(
-      <Route path="/" element={<App />}>
-        <Route path="" element={<Index />} loader={cheesesLoader} />
-        <Route path=":id" element={<Show />} loader={cheeseLoader}/>
+      
+      <Route path="/" element={<App/>}>
+        <Route path="" element={<Index/>} loader={cheesesLoader} />
+        <Route path=":id" element={<Show/>} loader={cheeseLoader}/>
         <Route path="create" action={createAction} />
         <Route path="update/:id" action={updateAction}/>
         <Route path="delete/:id" action={deleteAction}/>
